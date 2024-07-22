93042a0fed8030ced2273cf463a19db109b093cf
RBLucky <nkosi.s.lucky@gmail.com>
2024-07-22 16:28:42 +0200

    chapter9

---
+++ chapter projects/chapter8/project1/scramble.js
@@ -0,0 +1,22 @@
+"use strict";
+
+let word = "encyclopaedia";
+
+function scramble(string) {
+    let stringLength = string.length;
+
+    let tempString = "";
+
+    for (let i = 0; i < stringLength; ++i) {
+        let index = Math.floor(Math.random()*string.length);
+        let newLetter = string[index];
+        tempString += newLetter;
+
+        string = string.substr(0, index) + string.substr(index+1);
+        console.log(string);
+    }
+
+    return tempString;
+}
+
+console.log(scramble(word));
