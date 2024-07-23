3fd848852e9d8e5bb8b3392d42fb0e224fb1d45c
RBLucky <nkosi.s.lucky@gmail.com>
2024-07-23 18:29:41 +0200

    chapter10

---
+++ chapter projects/chapter10/project2/voting.js
@@ -0,0 +1,49 @@
+"use strict";
+
+let myArray = ["Brenda", "Felicia", "Steve", "Sipho", "Herman", "Lindi", "Thapelo"];
+
+let message = document.getElementById('message');
+let addFriend = document.getElementById('addFriend');
+let addnew = document.getElementById('addNew');
+let output = document.getElementById('output');
+
+addFriend.addEventListener("click", function () {
+    let input = this.value;
+
+    addToList(input, myArray.length, 0);
+    myArray.push(input);
+});
+
+function addToList(input, index, counter) {
+    let row = document.createElement("tr");
+    let firstCell = document.createElement("td");
+
+    firstCell.classList.add("box");
+
+    firstCell.textContent = index + 1;
+
+    const secondCell = document.createElement("td");
+    const thirdCell = document.createElement("td");
+
+    secondCell.textContent = input;
+    thirdCell.textContent = counter;
+
+    row.append(firstCell);
+    row.append(secondCell);
+    row.append(thirdCell);
+
+    row.onclick = function () {
+        console.log(row.lastChild);
+        let val = Number(row.lastChild.textContent);
+        val++;
+        row.lastChild.textContent = val;
+    }
+    output.appendChild(row);
+}
+
+
+function build() {
+    myArray.forEach((item, index) => {
+        addToList(item, index, 0);
+    });
+}
