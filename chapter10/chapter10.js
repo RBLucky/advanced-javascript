bc3c6c59772f1ebb8d3520e585848ad748586818
RBLucky <nkosi.s.lucky@gmail.com>
2024-07-23 10:28:04 +0200

    chapter10

--- chapter10/chapter10.js
+++ chapter10/chapter10.js
@@ -1,3 +1,15 @@
 "use strict";
 
-console.dir(document.body);
+// console.dir(document.body);
+
+// console.dir(document.body.children.forest.children.tree2.children.shrubbery.children.treasure);
+
+// console.dir(document.body.childNodes[3].childNodes[3].childNodes[1].childNodes[1]);
+
+// console.dir(document.body.children.forest.children.tree2.parentElement);
+// console.dir(document.body.children.forest.children.tree2);
+// console.dir(document.body.children.forest.children.tree2.previousElementSibling);
+// console.dir(document.body.children.forest.children.tree1.nextElementSibling);
+
+
+    
